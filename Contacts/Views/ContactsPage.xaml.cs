effb5eb58b46886ecc9724160e9e7652d2b0f557
gyuntekahmed <gyuntekahmed@abv.bg>
2024-03-31 11:56:02 +0300

    Add pages for add and edit contacts

---
+++ Contacts/Views/ContactsPage.xaml.cs
@@ -0,0 +1,24 @@
+namespace Contacts.Views;
+
+public partial class ContactsPage : ContentPage
+{
+	public ContactsPage()
+	{
+		InitializeComponent();
+	}
+
+    private void btnAddContact_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(AddContactPage));
+    }
+
+    private void btnEditContact_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(EditContactPage));
+    }
+
+    private void btnCancel_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
